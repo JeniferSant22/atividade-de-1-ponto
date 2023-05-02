1514199ad8a45e9c819b72014792fa5c08d5b3da
JeniferSant22 <125519506+JeniferSant22@users.noreply.github.com>
2023-05-02 16:07:45 -0300

    Update index.php

--- calculadoraexercicio/index.php
+++ calculadoraexercicio/index.php
@@ -7,34 +7,30 @@
 
     <!-- css boostrap -->
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">    
     <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-     
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>     
     <!-- css -->
     <link rel="stylesheet" href="css/style.css">
     <!-- font google -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap" rel="stylesheet">
 </head>
 <body>
     
     <div class="container">
-        <h1>Calculator</h1>
+        <h1>Calculadora</h1>
         <div id="calculator">
-        
-        </div>
-    </div>
-<form action=""method="POST">
+
+        <form action="" method="POST">
     <div id="display">
         <div id="results">
             Resultado:
             <!-- php -->
             <?php
                 if(isset($_POST['acao'])):
-                    $n1     =str_replace(',','.',$_POST['n1']);
+                    $n1     =str_replace(',','.', $_POST['n1']);
                     $n2     =str_replace(',','.',$_POST['n2']);
                     $tipo     =$_POST['tipo'];
 
@@ -42,20 +38,23 @@
                              $calcular = $n1 + $n2;
                         elseif($tipo == '-'): 
                             $calcular = $n1 - $n2;
-                        elseif($tipo == '/'): 
+                        elseif($tipo == '/'):
                             $calcular = $n1 / $n2;
-                        else:               
-                            $calcular = $n1 * $n2;
+                        else:
+                            $calcular = $n1 * $n2;   
                         endif;
-                        $calcular'='str_replace(',','.',$calcular);
+                        $calcular = str_replace('.',',', $calcular);
                         echo "$n1 $tipo $n2 igual a $calcular";
                      endif;
             ?>
             <!-- fim php -->
-                
         </div>
-    </div>    
-    <div id="keyboard">
+    </div>
+        </div>
+    </div>
+</body>
+
+<div id="keyboard">
         <div class="row">
             <div class="col-12">
                 <input type="text" name="n1" class="form-control mb-3" placeholder="Digite um número" required>
@@ -76,10 +75,5 @@
                  <button type="submit" class="btn btn-danger" name="acao">=</button>
             </div>
         </div>
-    </div>   
-</form>     
-
-
-
-</body>
-</html>
+</div>   
+</form>

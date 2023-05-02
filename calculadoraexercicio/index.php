1c21176e852f9df5971d07ef0664ed2485ff0721
JeniferSant22 <125519506+JeniferSant22@users.noreply.github.com>
2023-05-02 17:15:12 -0300

    Update index.php

--- calculadoraexercicio/index.php
+++ calculadoraexercicio/index.php
@@ -3,31 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculator</title>
+    <title>CALCULADORA</title>
 
-    <!-- css boostrap -->
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">    
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>     
-    <!-- css -->
-    <link rel="stylesheet" href="css/style.css">
-    <!-- font google -->
+    <!--CSS boostrap-->
+    <!--CSS only-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <!--JavaScript Sundle with pepper!-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <!--css-->
+    <link rel="stylesheet" href="style.css">
+    <!--font google-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap" rel="stylesheet">
 </head>
 <body>
-    
-    <div class="container">
-        <h1>Calculadora</h1>
-        <div id="calculator">
-
+    <div class="container" id="calculadora">
+        <h1>CALCULADORA</h1>
         <form action="" method="POST">
-    <div id="display">
-        <div id="results">
-            Resultado:
-            <!-- php -->
+            <div id="display">
+                <div id="results" >
+                    Resultado:
+              <!-- php -->
             <?php
                 if(isset($_POST['acao'])):
                     $n1     =str_replace(',','.', $_POST['n1']);
@@ -53,27 +50,29 @@
         </div>
     </div>
 </body>
-
-<div id="keyboard">
-        <div class="row">
-            <div class="col-12">
-                <input type="text" name="n1" class="form-control mb-3" placeholder="Digite um número" required>
+            <div id="keyboard">
+                <div class="row">
+                    <div class="col-12">
+                        <input type="text" name="n1" class="form-control mb-3" placeholder="Digite um número" required>
+                    </div>
+                    <div class="col-12">
+                        <select class="form select mb-3 bg-info" name="tipo" required>
+                            <option selected>SELECIONE UM OPERADOR</option>
+                            <option value="+">+</option>
+                            <option value="-">-</option>
+                            <option value="*">*</option>
+                            <option value="/">/</option>
+                        </select>
+                    </div>
+                    <div class="col-12">
+                        <input type="text" name="n2" class="form-control mb-3" placeholder="Digite um número" required>
+                    </div>
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-danger" name="acao">=</button>
+                    </div>
+                </div>
             </div>
-            <div class="col-12">
-                <select class="form-select mb-3 bg-info" name="tipo" required>
-                    <option selected>Selecione um operador</option>
-                    <option value="+">+</option>
-                    <option value="-">-</option>
-                    <option value="*">*</option>
-                    <option value="/">/</option>
-                </select>
-            </div>
-            <div class="col-12">
-                <input type="text" name="n2" class="form-control mb-3" placeholder="Digite um número" required>
-            </div>
-            <div class="col-12">
-                 <button type="submit" class="btn btn-danger" name="acao">=</button>
-            </div>
-        </div>
-</div>   
-</form>
+        </form>
+    </div>
+</body>
+</html>
